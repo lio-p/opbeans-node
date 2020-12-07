7274ea029824c0698f21245dbaf00d7fd9c2d9e4
Lionel Palacin <lionel.palacin@gmail.com>
2020-12-07 12:45:10 +0000

    change products for hipster

--- db/products.sql
+++ db/products.sql
@@ -2,12 +2,23 @@ insert into product_types (id, name) values (1, 'Light Roast Coffee');
 insert into product_types (id, name) values (2, 'Medium Roast Coffee');
 insert into product_types (id, name) values (3, 'Dark Roast Coffee');
 
-insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (1, 'OP-DRC-C1', 'Brazil Verde, Italian Roast', 'Soft, nutty, low acid, with nice bitter-sweet chocolate tastes.', 3, '80', 1500, 3200);
-insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (2, 'OP-MRC-C2', 'Jamaica Blue Mountain, Vienna Roast', 'Rich flavor, rich aroma, moderate acidity, and an even balance.', 2, '0', 20000, 42000);
-insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (3, 'OP-LRC-C3', 'Colombian Supremo, Cinnamon Roast', 'Full bodied with a light acidity making a balanced cup.', 1, '150', 2000, 4200);
-insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (4, 'OP-LRC-C4', 'Guatemala Antigua, New England Roast', 'Lively acidity, complex spiciness, and chocolate laced aftertaste.', 1, '0', 1300, 2900);
-insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (5, 'OP-MRC-C5', 'Ethiopian Moka Java, Breakfast Roast', 'Possess an intense floral bouquet and create a pleasant cup of coffee.', 2, '130', 1800, 3900);
-insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (6, 'OP-DRC-C6', 'European Royale, French Roast', 'Begins full, mellows as it lingers and then finishes with a smooth sweet aftertaste.', 3, '70', 1200, 2500);
-insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (7, 'OP-MRC-C7', 'Hawaiian Kona, Medium Roast', 'A rich, rounded cup with superb fragrance and flavor of Kona.', 2, '60', 2100, 3200);
-insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (8, 'OP-LRC-C8', 'Papua New Guinea Arokara, Light City Roast', 'Sweet aroma, round body, lively acidity.', 1, '90', 1400, 2900);
-insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (9, 'OP-DRC-C9', 'Bali Blue Moon, French Roast', 'A classic clean cup with great body and mildness.', 3, '70', 1200, 2200);
+-- insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (1, 'OP-DRC-C1', 'Brazil Verde, Italian Roast', 'Soft, nutty, low acid, with nice bitter-sweet chocolate tastes.', 3, '80', 1500, 3200);
+-- insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (2, 'OP-MRC-C2', 'Jamaica Blue Mountain, Vienna Roast', 'Rich flavor, rich aroma, moderate acidity, and an even balance.', 2, '0', 20000, 42000);
+-- insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (3, 'OP-LRC-C3', 'Colombian Supremo, Cinnamon Roast', 'Full bodied with a light acidity making a balanced cup.', 1, '150', 2000, 4200);
+-- insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (4, 'OP-LRC-C4', 'Guatemala Antigua, New England Roast', 'Lively acidity, complex spiciness, and chocolate laced aftertaste.', 1, '0', 1300, 2900);
+-- insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (5, 'OP-MRC-C5', 'Ethiopian Moka Java, Breakfast Roast', 'Possess an intense floral bouquet and create a pleasant cup of coffee.', 2, '130', 1800, 3900);
+-- insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (6, 'OP-DRC-C6', 'European Royale, French Roast', 'Begins full, mellows as it lingers and then finishes with a smooth sweet aftertaste.', 3, '70', 1200, 2500);
+-- insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (7, 'OP-MRC-C7', 'Hawaiian Kona, Medium Roast', 'A rich, rounded cup with superb fragrance and flavor of Kona.', 2, '60', 2100, 3200);
+-- insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (8, 'OP-LRC-C8', 'Papua New Guinea Arokara, Light City Roast', 'Sweet aroma, round body, lively acidity.', 1, '90', 1400, 2900);
+-- insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (9, 'OP-DRC-C9', 'Bali Blue Moon, French Roast', 'A classic clean cup with great body and mildness.', 3, '70', 1200, 2200);
+
+
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (1, 'OP-DRC-C1', 'Vintage Typewriter', 'This typewriter looks good in your living room.', 3, '80', 67, 130);
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (2, 'OP-MRC-C2', 'Vintage Camera Lens', 'You will not have a camera to use it and it probably does not work anyway.', 2, '0', 12, 24);
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (3, 'OP-LRC-C3', 'Home Barista Kit', 'Always wanted to brew coffee with Chemex and Aeropress at home ?', 1, '150', 124, 240);
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (4, 'OP-LRC-C4', 'Terrarium', 'This terrarium will looks great in your white painted living room.', 1, '0', 36, 72);
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (5, 'OP-MRC-C5', 'Film Camera', 'This camera looks like it is a film camera, but it is actually digital.', 2, '130', 2245, 4200);
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (6, 'OP-DRC-C6', 'Vintage Record Player', 'It still works.', 3, '70', 65, 130);
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (7, 'OP-MRC-C7', 'Metal Camping Mug', 'You probably do not go camping that often but this is better than plastic cups.', 2, '60', 24, 48);
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (8, 'OP-LRC-C8', 'City Bike', 'This single gear bike probably cannot climb the hills of San Francisco.', 1, '90', 789, 1500);
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (9, 'OP-DRC-C9', 'Air Plant', 'Have you ever wondered whether air plants need water? Buy one and figure out.', 3, '70', 12, 24);
